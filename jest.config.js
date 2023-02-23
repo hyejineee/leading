088f31caf43dd52466e38a7f624eebfc4c639b70
hyejineee <dev.hyejin04@gmail.com>
2023-02-23 17:17:41 +0900

    settings : jest 설정

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  preset: 'ts-jest',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['node_modules'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    'jest-plugin-context/setup',
+  ],
+  verbose: true,
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ pages/app.test.tsx
@@ -0,0 +1,7 @@
+export {};
+
+describe('test', ()=>{
+  test('', ()=>{
+    expect(1 + 1).toBe(2);
+  });
+});
