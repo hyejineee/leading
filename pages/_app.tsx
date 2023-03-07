0497e9c858d58cf7ef1e96cfd14061f6489635b5
hyejineee <dev.hyejin04@gmail.com>
2023-03-07 14:55:29 +0900

    chore : inversify 패키지 추가 및 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import 'reflect-metadata';
 import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import GlobalStyles from '../src/common/styles/globalStyles';
@@ -6,11 +7,11 @@ import useTheme from '../src/common/styles/hooks/useTheme';
 import mode from '../src/common/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [theme ] = useTheme();
+  const [theme] = useTheme();
 
   return (
     <ThemeProvider theme={mode[theme]}>
-      <Global styles={GlobalStyles}/>
+      <Global styles={GlobalStyles} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
