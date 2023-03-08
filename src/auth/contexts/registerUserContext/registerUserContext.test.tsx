69e0def828aa8628e877114ef59477a1e33b8958
hyejineee <dev.hyejin04@gmail.com>
2023-03-09 00:13:48 +0900

    feat : 회원가입 페이지 context 생성 및 테스트

---
+++ src/auth/contexts/registerUserContext/registerUserContext.test.tsx
@@ -0,0 +1,67 @@
+// 회원가입 페이지에서 사용되는 상태를 관리하는 context
+
+import RegisterUserUseCase from '@auth/usecases/RegisterUserUseCase/RegisterUserUseCase';
+import { act, render, renderHook } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { anything, instance, mock, when } from 'ts-mockito';
+import RegisterUserPageState from './pageState';
+import {
+  RegisterUserProvider,
+  useRegisterUser,
+  useRegisterUserPageState,
+} from './registerUserContext';
+
+describe('RegisterUserContext', () => {
+  const mockRegisterUserUseCase = mock<RegisterUserUseCase>();
+
+  const customRender = (registerUserUseCase: RegisterUserUseCase) => {
+    const wrapper = ({ children }: { children: ReactNode }) => (
+      <RegisterUserProvider registerUserUseCase={registerUserUseCase}>
+        {children}
+      </RegisterUserProvider>
+    );
+
+    return renderHook(
+      () => ({
+        registerUser: useRegisterUser(),
+        registerPageState: useRegisterUserPageState(),
+      }),
+      { wrapper },
+    );
+  };
+
+  it('회원가입에 성공하면, 회원가입 성공 state를 발행한다.', async () => {
+    when(
+      mockRegisterUserUseCase.execute(anything(), anything(), anything()),
+    ).thenResolve();
+
+    const { result } = customRender(instance(mockRegisterUserUseCase));
+
+    await act(async () => {
+      await result.current.registerUser('email', 'password', 'username');
+    });
+
+    expect(result.current.registerPageState?.name).toBe(
+      RegisterUserPageState.SUCCESS_REGISTER_USER,
+    );
+  });
+
+  it('회원가입에 실패하면, 회원가입 실패 state를 발행한다.', async () => {
+    const errorMessage = '회원가입에 실패했습니다.';
+
+    when(
+      mockRegisterUserUseCase.execute(anything(), anything(), anything()),
+    ).thenThrow(Error(errorMessage));
+
+    const { result } = customRender(instance(mockRegisterUserUseCase));
+
+    await act(async () => {
+      await result.current.registerUser('email', 'password', 'username');
+    });
+
+    expect(result.current.registerPageState?.name).toBe(
+      RegisterUserPageState.FAIL_REGISTER_USER,
+    );
+    expect(result.current.registerPageState?.message).toBe(errorMessage);
+  });
+});
--- src/auth/types/index.ts
+++ src/auth/types/index.ts
@@ -1,6 +1,8 @@
-import { UseLogin } from '@auth/types/hooks.types';
+import RegisterUserPageState from '@auth/contexts/registerUserContext/pageState';
+import { UseLogin, UseRegisterUser } from '@auth/types/hooks.types';
 import { UserEntity } from '@common/entities/user';
 import { SuccessResponse } from '@common/network/types/apiResponse';
+import Action from '@common/types/Action';
 
 export interface IAuthRepository {
   login(email: string, password: string): Promise<SuccessResponse<UserEntity>>;
@@ -13,4 +15,9 @@ export interface IAuthRepository {
 
 export interface IAuthContext {
   login: UseLogin;
+}
+
+export interface IRegisterUserContext {
+  uiState: Action<RegisterUserPageState, null> | null;
+  registerUser: UseRegisterUser;
 }
